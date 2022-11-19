016d56f502c8048ca93cb52563728f707800514b
JesusDM404 <96702406+JesusDM404@users.noreply.github.com>
2022-11-19 16:40:12 -0600

    Add files via upload

---
+++ Polindromo-palabra/Polindromo.js
@@ -0,0 +1,16 @@
+
+function espolindomo(){
+    let cadena = $("#texto").val();
+    let array = cadena.split("");
+    let invertir = array.reverse();
+    document.getElementById("palabra").innerHTML =cadena == invertir.join("")? "Si es polindromo y seria asi  "+ cadena.split('').reverse().join(''): "no es polindromo";
+    // alert(cadena.split('').reverse().join(''));
+}
+
+
+
+// var valor1 = prompt("Ingrese un valor");
+
+// valor1 = espolindomo(valor1)
+
+
